1458b87851aac5045e627b5acf417404dd0215ff
ychan43 <43016132+ychan43@users.noreply.github.com>
2018-09-19 16:54:26 -0500

    Project Commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
-public class Main {
+import javafx.scene.web.WebEngine;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println(WebScrapper.urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        String doc = WebScrapper.urlToString("http://erdani.com/tdpl/hamlet.txt");
+        String[] words = doc.split(" ");
+        int counter = 0;
+        for (int c = 0; c < words.length; c++) {
+            if (words[c] != " ") {
+                counter++;
+            }
+        }
+        System.out.print(counter);
     }
 }
